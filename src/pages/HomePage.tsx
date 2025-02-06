f3a6038cc89149f26264e86df34573e0dbf11057
Thor <thorlhansen@hotmail.com>
2025-02-06 18:38:26 +0100

    deleted button from testing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getMeals } from '../services/firebase';
 import { Meal } from '../models/Meal';
-import { getFoodComponents } from '../services/firebase';
 
 function HomePage() {
   const [meals, setMeals] = useState<Meal[]>([]);
@@ -10,20 +9,9 @@ function HomePage() {
     getMeals().then(data => setMeals(data));
   }, []);
 
-  const handleClick = () => {
-    console.log("Click");
-    getFoodComponents()
-  };
 
   return (
     <div className="p-4">
-      <button
-        type="button"
-        onClick={handleClick}
-        className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-      >      
-      Alternative
-    </button>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {meals.map(meal => (
           <div key={meal.id} className="card">
