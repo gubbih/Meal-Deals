aa9eff57b97ccbdbd216fdf8ed280cdfa2b73f49
Thor <thorlhansen@hotmail.com>
2025-02-17 16:13:35 +0100

    white space added and prettier

--- src/components/MealForm.tsx
+++ src/components/MealForm.tsx
@@ -80,7 +80,7 @@ const MealForm: React.FC<MealFormProps> = ({
           </label>
           <textarea
             name="description"
-            value={defaultMeal.description}
+            defaultValue={defaultMeal.description}
             onChange={onInputChange}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
